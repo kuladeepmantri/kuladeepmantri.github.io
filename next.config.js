d58c5336b5b802ffe0d24428055fb7d9d44f7c62
Kuladeep Mantri <mantrikuladeep@gmail.com>
2024-03-13 03:29:37 -0400

    Retry-2

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // output: 'export',
-    // assetPrefix: './',  // Add this line
-    basePath: '', // Add this line
- }
- 
- module.exports = nextConfig
- 
