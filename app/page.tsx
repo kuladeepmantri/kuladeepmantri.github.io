b5afb67c6e93d26705e27fbbcd15faae7d4a9c53
Kuladeep Mantri <mantrikuladeep@gmail.com>
2024-03-13 03:27:03 -0400

    Portfoli-2

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GridSection from "@/containers/home/GridSection";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <GridSection />
+    </React.Fragment>
+  );
+}
